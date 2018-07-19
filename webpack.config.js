1ff5ee784c37c7edba6b4b69b21c2a03bbe65b95
aurora <18862324237@163.com>
2018-07-19 11:24:32 +0800

    下载vue组件，配置vue支持

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,32 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var config = {
     entry: {
-        main: './main'  // main.js
+        main: './main'  // main.js 入口文件
     },
     output: {
         path: path.join(__dirname, './dist'),
-        publicPath: 'dist',
-        filename: 'main.js'
+        publicPath: '/dist/',
+        filename: 'main.js'     // 出口文件
     },
     module: {
         rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        css: ExtractTextPlugin.extract({
+                            use: 'css-loader',
+                            fallback: 'vue-style-loader'
+                        })
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
             {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
