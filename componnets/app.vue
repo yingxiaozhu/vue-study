0499fb2218f4bc58bd28dba5d5943093107975da
aurora <18862324237@163.com>
2018-07-19 14:54:10 +0800

    增加对各种文件的打包支持

--- componnets/app.vue
+++ componnets/app.vue
@@ -2,6 +2,9 @@
     <div>
         <v-title title="Vue 组件化"></v-title>
         <v-button @click="handleClick">点击按钮</v-button>
+        <p>
+            <img src="../images/image.png" style="width: 200px;">
+        </p>
     </div>
 </template>
 
