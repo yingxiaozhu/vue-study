eb32ebcb220904780e177088090e23aa2206913d
aurora <18862324237@163.com>
2018-07-19 14:18:20 +0800

    增加vue组件

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>Hello {{ name }}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: "Vue.js"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        color: #f60;
+        font-size: 24px;
+    }
+</style>
--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-import './style.css';
+//import './style.css';
+// 导入 VUe 框架
+import Vue from 'vue';
+// 导入 app.vue 组件
+import App from './app.vue';
 
-document.getElementById('app').innerHTML = 'Hello webpack!';
+// 创建 Vue 根实例
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
+
+//document.getElementById('app').innerHTML = 'Hello webpack!';
